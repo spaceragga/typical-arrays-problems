0b984a0e25702674ae170e67e866bf8370f1d534
spaceragga <56797618+spaceragga@users.noreply.github.com>
2020-10-01 22:52:28 +0300

    init: start typical-arrays-problems-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  } else {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  }
 }
